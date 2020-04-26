04e4e31278f0c23bee88656f36a161610086ed2a
Guru Prasad Srinivasa <gurupras@buffalo.edu>
2020-04-26 13:46:55 -0400

    Added sources

---
+++ src/log.js
@@ -0,0 +1,125 @@
+const appRoot = require('app-root-path')
+const { createLogger, format, transports } = require('winston')
+require('winston-daily-rotate-file')
+const deepmerge = require('deepmerge')
+const moment = require('moment')
+
+const { combine, colorize, simple, printf } = format
+const colorizer = colorize()
+
+const TAG_SYMBOL = '__tag__'
+
+// define the custom settings for each transport (file, console)
+var options = {
+  file: {
+    level: 'debug',
+    dirname: `${appRoot}/logs`,
+    filename: 'log-%DATE%.log',
+    handleExceptions: true,
+    json: true,
+    zippedArchive: true,
+    maxsize: 10 * 1024 * 1024, // 10MB
+    colorize: false
+  },
+  console: {
+    level: 'debug',
+    handleExceptions: true,
+    json: false,
+    colorize: true
+  }
+}
+
+// instantiate a new Winston Logger with the settings defined above
+const consoleLogger = createLogger({
+  prettyPrint: true,
+  format: combine(
+    simple(),
+    printf((msg) => {
+      // Update msg.timestamp to be local
+      msg.timestamp = moment().local().format('YYYY-MM-DD hh:mm:ss.SSS ZZ')
+
+      const splat = msg[Symbol.for('splat')]
+      let obj = {}
+      const result = []
+      if (Array.isArray(splat)) {
+        splat.forEach((entry) => {
+          if (typeof entry === 'object') {
+            obj = deepmerge(obj, entry)
+          } else {
+            result.push(entry)
+          }
+        })
+      } else {
+        console.warn(`splat was not an array: ${JSON.stringify(msg)}`)
+      }
+      const { [TAG_SYMBOL]: tag } = obj
+      delete obj[TAG_SYMBOL]
+      result.unshift([
+        `${msg.timestamp} - ${(msg.level + ':').padEnd(8, ' ')} ${tag.padEnd(
+          20,
+          ' '
+        )} ${msg.message}`
+      ])
+      if (Object.keys(obj).length > 0) {
+        result.push(JSON.stringify(obj))
+      }
+      const string = result.join(' ')
+      return colorizer.colorize(msg.level, string)
+    })
+  ),
+  transports: [new transports.Console(options.console)],
+  exitOnError: false // do not exit on handled exceptions
+})
+
+const fileLogger = createLogger({
+  transports: [new transports.DailyRotateFile(options.file)]
+})
+
+const loggers = [consoleLogger, fileLogger]
+
+class Logger {
+  constructor (tag, defaultLevel = 'info') {
+    Object.assign(this, {
+      tag,
+      defaultLevel
+    })
+  }
+
+  log (...args) {
+    this.__log(this.defaultLevel, ...args)
+  }
+
+  __log (level, ...args) {
+    const { tag } = this
+    loggers.forEach((logger) => logger[level](...args, { [TAG_SYMBOL]: tag }))
+  }
+
+  error (...args) {
+    this.__log('error', ...args)
+  }
+
+  warn (...args) {
+    this.__log('warn', ...args)
+  }
+
+  info (...args) {
+    this.__log('info', ...args)
+  }
+
+  verbose (...args) {
+    this.__log('verbose', ...args)
+  }
+
+  debug (...args) {
+    this.__log('debug', ...args)
+  }
+
+  silly (...args) {
+    this.__log('silly', ...args)
+  }
+}
+
+module.exports = {
+  Logger,
+  loggers
+}
