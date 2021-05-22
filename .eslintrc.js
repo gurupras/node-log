15a78900db34241713f0901a3bf8fb3442a314dc
Guru Prasad Srinivasa <gurupras@buffalo.edu>
2021-05-22 00:36:33 -0400

    Updated all packages

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,15 @@
 module.exports = {
   env: {
     browser: true,
-    commonjs: true,
-    es6: true
+    es2021: true
   },
   extends: [
     'standard',
     'plugin:jest/recommended'
   ],
-  globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly'
-  },
   parserOptions: {
-    ecmaVersion: 2018
+    ecmaVersion: 12,
+    sourceType: 'module'
   },
   rules: {
   }
