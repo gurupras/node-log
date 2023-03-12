edad94d2d4950f6c60a6de9d65da570ab739c962
Guru Prasad Srinivasa <gurupras@buffalo.edu>
2023-03-11 21:19:07 -0500

    Fixes bug where customLevels only had silly instead of extending existing levels

--- src/log.ts
+++ src/log.ts
@@ -124,7 +124,13 @@ function initialize (config: Config = {}) {
       targets
     },
     customLevels: {
-      silly: 5
+      silly: 5,
+      trace: 10,
+      debug: 20,
+      info: 30,
+      warn: 40,
+      error: 50,
+      fatal: 60
     },
     level
   })
